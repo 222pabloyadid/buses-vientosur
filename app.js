b6731d6e93f10efc86b5ee740d62dc4f58556f36
Pablo Yañez <pyanez089@gmail.com>
2026-04-25 22:01:58 -0400

    fix cors real

--- app.js
+++ app.js
@@ -177,8 +177,18 @@ app.use(cors({
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]
 }));
-app.options('/*', cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://busevientosur.cl");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 app.use(express.json());
 
 app.use(express.static("public"));
